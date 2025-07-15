cdc2c06041eadc05767525976987079300596832
Rodrigo Soares <soaresjw27@gmail.com>
2025-07-14 21:08:30 -0300

    metodo detalhar topico concluido

--- src/main/java/challenge/forum/alura/controller/TopicoController.java
+++ src/main/java/challenge/forum/alura/controller/TopicoController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("topico")
@@ -19,18 +22,27 @@ public class TopicoController {
   @Autowired
   private TopicoRepository repository;
 
-    @PostMapping
-    @Transactional
-    public void cadastrar(@RequestBody @Valid CadastroTopicoDTO dados) {
-        repository.save(new Topico(dados));
-    }
+  @PostMapping
+  @Transactional
+  public void cadastrar(@RequestBody @Valid CadastroTopicoDTO dados) {
+    repository.save(new Topico(dados));
+  }
 
   @GetMapping
-  public Page<ListagemTopicoDTO> listar(@PageableDefault(size = 10, sort=("titulo")) Pageable paginacao){
+  public Page<ListagemTopicoDTO> listar(@PageableDefault(size = 10, sort = ("titulo")) Pageable paginacao) {
     return repository.findAll(paginacao).map(ListagemTopicoDTO::new);
 
   }
 
+  @GetMapping("/{id}")
+  @Transactional(readOnly = true)
+  public ResponseEntity<ListagemTopicoDTO> detalhe(@PathVariable Long id) {
+    Optional<Topico> topico = repository.findById(id);
+    return topico
+            .map(t -> ResponseEntity.ok(new ListagemTopicoDTO(t)))
+            .orElseGet(() -> ResponseEntity.notFound().build());
+
+  }
 }
 
 
